404b289778d636c909cc366ceca2781e4618e1f6
Eduardo <eduhenriquesrs@gmail.com>
2022-04-16 12:15:12 -0300

    Adicionado o uso de Cache aos topicos.

--- Curso Spring Security/forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ Curso Spring Security/forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -9,6 +9,7 @@ import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -37,9 +38,9 @@ public class TopicosController {
     private CursoRepository cursoRepository;
 
     @GetMapping
-
+    @Cacheable(value = "listaDeTopicos")
     public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso,
-                                 @PageableDefault(sort = "id", direction = Sort.Direction.ASC, page = 0, size = 1) Pageable paginacao) {
+                                 @PageableDefault(sort = "id", direction = Sort.Direction.ASC, page = 0, size = 10) Pageable paginacao) {
 
         if (nomeCurso == null) {
             Page<Topico> topicos = topicoRepository.findAll(paginacao);
