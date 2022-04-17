295c7934f1cfb751960206cbfcd0ee2714a3edb1
Eduardo <eduhenriquesrs@gmail.com>
2022-04-17 00:59:33 -0300

    Gerando tokens com JWT.

--- Curso Spring Security/forum/src/main/java/br/com/alura/forum/config/validacao/security/AutenticacaoService.java
+++ Curso Spring Security/forum/src/main/java/br/com/alura/forum/config/validacao/security/AutenticacaoService.java
@@ -19,9 +19,12 @@ public class AutenticacaoService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         Optional<Usuario> usuario = repository.findByEmail(username);
-        if(usuario.isPresent()) {
+        if (usuario.isPresent()) {
             return usuario.get();
         }
-        throw new UsernameNotFoundException("Dados invalidos");
+
+        throw new UsernameNotFoundException("Dados inválidos!");
     }
+
 }
+
--- Curso Spring Security/forum/src/main/java/br/com/alura/forum/config/validacao/security/SecurityConfigurations.java
+++ Curso Spring Security/forum/src/main/java/br/com/alura/forum/config/validacao/security/SecurityConfigurations.java
@@ -1,8 +1,10 @@
 package br.com.alura.forum.config.validacao.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -18,26 +20,35 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
     @Autowired
     private AutenticacaoService autenticacaoService;
 
-    //Configurações de autenticacao
+    @Override
+    @Bean
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+
+    //Configuracoes de autenticacao
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(autenticacaoService).passwordEncoder(new BCryptPasswordEncoder());
     }
-    //Configurações de Autorizacao
+
+    //Configuracoes de autorizacao
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers(HttpMethod.GET,"/topicos").permitAll()
-                .antMatchers(HttpMethod.GET, "topicos/*").permitAll()
+                .antMatchers(HttpMethod.GET, "/topicos").permitAll()
+                .antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
+                .antMatchers(HttpMethod.GET, "/h2-console").permitAll()
                 .antMatchers(HttpMethod.POST, "/auth").permitAll()
                 .anyRequest().authenticated()
                 .and().csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
-    //Configurações de recursos estaticos(js, css, images, etc...)
+
+
+    //Configuracoes de recursos estaticos(js, css, imagens, etc.)
     @Override
     public void configure(WebSecurity web) throws Exception {
-        super.configure(web);
     }
 
 }
